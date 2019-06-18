a3c80ff8837c78cb8a591b41fb9b58760995303d
Lane P <itislp@icloud.com>
2019-06-18 14:03:34 -0500

    updated layout, created Call component

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
- 	siteName: 'Gridsome',
+ 	siteName: 'Toast Masters | Hays,KS',
 	plugins: [
 		{
 			use: 'gridsome-plugin-tailwindcss'
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,36 +1,7 @@
 <template>
   <Layout>
-    
-    <!--call to action-->
-    <div class="flex flex-wrap mt-12 relative">
-      <div class="w-full sm:w-full md:w-1/2">
-        <p class="text-sm uppercase text-gray-500 font-semibold">
-          Toast Masters Hays, KS
-        </p>
-        <h1 class="text-4xl font-bold text-gray-900">
-          Practice Public Speaking
-        </h1>
-        <p class="text-gray-700 pr-8 text-lg">
-          Practice your public speaking with like minded speakers. Build confidence in the 
-          work place, socially, and create a better you.
-        </p>
-        <p class="mt-8 text-gray-700 text-md">
-          Sign up today! Come join us in a meeting without signing up to see if its right for you.
-        </p>
-        <div class="flex mt-2">
-          <input class="w-2/3 border border-gray-400 rounded-md focus:outline-none p-2 mr-2 hover:border-gray-500 focus:border-gray-500" placeholder="speaker@example.com" type="email">
-          <button class="flex-1 bg-red-700 text-white outline-none">
-            Sign Up
-          </button>
-        </div>
-      </div>
-      <div class="w-full sm:w-full md:w-1/3 p-6 sm:p-6 md:pl-12">
-        <g-image class="md:absolute md:w-4/5" alt="Person public speaking" src="~/assets/conference_speaker.svg"/>
-      </div>
-    </div>
-
     <!--description-->
-    <div class="flex flex-wrap-reverse md:mt-48 bg-white relative items-center">
+    <div class="flex flex-wrap-reverse md:mt-24 bg-white relative items-center">
       <div class="w-full sm:w-full md:w-3/5">
         <div>
           <h2 class="text-3xl leading-tight text-gray-900 font-bold">
@@ -82,7 +53,7 @@
             <div class="w-12">
               <svg class="w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="261"><polyline points="20 6 9 17 4 12"/></svg>
             </div>
-            <div class="flex-1">Increase perspiration</div>
+            <div class="flex-1">Increased perspiration</div>
           </div>
 
           <!--item-->
@@ -158,18 +129,13 @@
             </div>
             <div class="flex-1">and much more!</div>
           </div>
-
         </div>
       </div>
     </div>
-
+    <footer class='mt-12 w-full text-center border-t border-grey-500 p-4'>
+    <p class="text-sm uppercase text-gray-500 font-semibold">
+        © {{ new Date().getFullYear() }} Toast Masters | Hays,KS
+      </p>   
+    </footer>
   </Layout>
 </template>
-
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
-</script>
