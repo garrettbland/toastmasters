2b1e73c6b134703789260d73f1e66a531b02f626
Lane P <itislp@icloud.com>
2019-06-18 21:40:47 -0500

    added footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class='mt-12 w-full text-center border-t border-grey-500 p-4'>
+    <p class="text-sm uppercase text-gray-500 font-semibold">
+        © {{ new Date().getFullYear() }} Toast Masters | Hays,KS
+      </p>   
+    </footer>    
+</template>
+
+<script>
+export default {
+	name:'Call',
+	data(){
+		return {
+			display:false
+		}
+	}
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -132,10 +132,5 @@
         </div>
       </div>
     </div>
-    <footer class='mt-12 w-full text-center border-t border-grey-500 p-4'>
-    <p class="text-sm uppercase text-gray-500 font-semibold">
-        © {{ new Date().getFullYear() }} Toast Masters | Hays,KS
-      </p>   
-    </footer>
   </Layout>
 </template>
